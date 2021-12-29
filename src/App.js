70b447a23b6360389b251af5ce6de99a9d396aca
iamkupoe <samuelkupoe1@gmail.com>
2021-12-29 08:34:17 -0600

    Dark Theme component created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import useLocalStorage from 'use-local-storage'
 
 function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-theme={theme}>
+      <span>Easy Darkmode and Themes in React</span>
+      <button onClick={switchTheme}>
+         {theme === 'light' ? 'Dark' : 'light'} 
+      </button>
     </div>
   );
 }
