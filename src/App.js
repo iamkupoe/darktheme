3825eb2236a9697bccf0109b54f3c29ce5e1a005
iamkupoe <samuelkupoe1@gmail.com>
2021-12-29 09:31:18 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import './App.css';
-import useLocalStorage from 'use-local-storage'
+import useLocalStorage from 'use-local-storage';
+import DarkModeToggle from "react-dark-mode-toggle";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(() => false);
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
   const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
 
@@ -11,12 +13,18 @@ function App() {
     setTheme(newTheme);
   }
   return (
+    
     <div className="App" data-theme={theme}>
-      <span>Easy Darkmode and Themes in React</span>
-      <button onClick={switchTheme}>
+    {/*<DarkModeToggle
+    onChange={setIsDarkMode}
+    checked={isDarkMode}
+    size={80}
+    />*/}
+      <button onClick={switchTheme} style={{width: 50, height: 50, borderRadius: "50%"}}>
          {theme === 'light' ? 'Dark' : 'light'} 
       </button>
     </div>
+   
   );
 }
 
